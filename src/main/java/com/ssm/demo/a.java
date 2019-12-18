57f84e5687436f6fdc33cfd5103b38053639e13c
ASUS <812149295>
2019-12-18 23:01:39 +0800

    a

---
+++ src/main/java/com/ssm/demo/a.java
@@ -0,0 +1,4 @@
+package com.ssm.demo;
+
+public class a {
+}
---
+++ src/main/java/com/ssm/demo/util/MyMapper.java
@@ -0,0 +1,18 @@
+package com.ssm.demo.util;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+import java.util.List;
+
+/**
+ * @author ASUS
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
+    /**
+     * 无参获取全部的方法
+     * @return
+     */
+    @Override
+    List<T> selectAll();
+}
